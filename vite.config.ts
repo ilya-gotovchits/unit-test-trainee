40a698808d950ff18c37cf6f3f4db16ce8cdcc33
ilya-gotovchits <ilya.hatouchyts@efficiently.com>
2025-11-26 16:08:10 +0300

    [tests] add tests coverage

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,11 @@ export default defineConfig({
     globals: true,
     environment: 'node',
     include: ['**/*.spec.ts'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html', 'lcov'],
+      reportsDirectory: 'coverage',
+      exclude: ['src/main.ts'],
+    },
   },
 });
